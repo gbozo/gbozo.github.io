dc67b813134eedecdbae8387b5c1384c689e8a96
George Bozouris <4042540+gbozo@users.noreply.github.com>
2023-09-15 12:10:07 +0300

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -1,16 +1,15 @@
 import configparser
 import re
 import requests
-from datetime import datetime, timedelta
-import os
 
 CONFIG_PATH = 'iptv.ini'
-CACHE_PATH = '/tmp/m3u8_cache/m3u8_data'
-CACHE_EXPIRATION = timedelta(seconds=300)
 
 # Load configuration file
 config = configparser.ConfigParser()
 config.read(CONFIG_PATH)
+
+# Retrieve epgurl
+epgurl = config['General']['epgurl']
 
 # URLs section
 urls = sorted(config['URLs'].items(), key=lambda x: int(re.search(r'\d+', x[0]).group()))
@@ -18,8 +17,9 @@ urls = sorted(config['URLs'].items(), key=lambda x: int(re.search(r'\d+', x[0]).
 # Stream title filters section
 title_filters = sorted(config['StreamTitleFilters'].items(), key=lambda x: int(re.search(r'\d+', x[0]).group()))
 
+
 def generate_m3u8():
-    output = "#EXTM3U url-tvg=\"epg.xml\"\n"
+    output = f"#EXTM3U url-tvg=\"{epgurl}\"\n"
     global_unique_m3u8 = set()
 
     for key, url_line in urls:
@@ -27,6 +27,7 @@ def generate_m3u8():
         url = url_parts[0].strip() if len(url_parts) > 0 else None
         custom_stream_name = url_parts[1].strip() if len(url_parts) > 1 else None
         epgname = url_parts[2].strip() if len(url_parts) > 2 else None
+
         response = requests.get(url)
         if not response.ok:
             continue
@@ -38,8 +39,10 @@ def generate_m3u8():
             stream_name_match = re.search(r'<title>([^<]+)<\/title>', content, re.I)
             stream_name = stream_name_match.group(1) if stream_name_match else 'Unknown Stream'
             stream_name = ''.join(filter(lambda x: ord(x) < 128, stream_name))
+            
             for _, filter_ in title_filters:
                 stream_name = re.sub(r'\b' + re.escape(filter_) + r'\b', '', stream_name, flags=re.I)
+            
             stream_name = stream_name.replace('|', '').strip()
 
         lc_stream_name = stream_name.lower()
